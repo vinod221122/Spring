df1e6cdb93cc57a8eed0733b67400278c1d30d34
VinodHoney Kusuma <VinodHoney Kusuma@DESKTOP-OH8RMEM>
2018-01-27 11:30:39 +0530

    intial check for restControllerDemo

---
+++ RestGit/src/main/java/com/rest/RestControllerDemo.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author VinodHoney Kusuma
+ *
+ */
+@RestController
+public class RestControllerDemo {
+private void hello() {
+	// TODO Auto-generated method stub
+System.out.println("hello");
+}
+}
