807041ce0cedf1c79f7949602475214d44b17983
VinodHoney Kusuma <VinodHoney Kusuma@DESKTOP-OH8RMEM>
2018-01-27 11:28:48 +0530

    restDemo intial check.

---
+++ RestGit/src/main/java/com/rest/demo/RestDemo.java
@@ -0,0 +1,16 @@
+package com.rest.demo;
+
+/**
+ * @author VinodHoney Kusuma
+ *
+ */
+public class RestDemo {
+
+	/**
+	 * 
+	 */
+	private void hello1() {
+		// TODO Auto-generated method stub
+		System.out.println("hello1");
+	}
+}
